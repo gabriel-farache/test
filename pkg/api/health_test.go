f42d37477f2a585ec4fefb21a8bb5713b07e3697
gabriel-farache <gfarache@redhat.com>
2025-02-19 14:51:12 +0100

    Updating file: pkg/api/health_test.go

---
+++ pkg/api/health_test.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestHealth(t *testing.T) {
+	// Create a new HTTP request
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v", err)
+	}
+
+	// Create a response recorder
+	w := httptest.NewRecorder()
+
+	// Create a context for the request
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	// Call the function we want to test
+	Health(c)
+
+	// Check the status code is what we expect
+	if w.Code != http.StatusOK {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+
+	// Check the response body is what we expect
+	expected := `{"healthy":true}`
+	if w.Body.String() != expected {
+		t.Fatalf("Expected to get '%s' but instead got '%s'\n", expected, w.Body.String())
+	}
+}
